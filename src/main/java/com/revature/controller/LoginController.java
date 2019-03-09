9deace194ce707a26e7d4b2589600fc44781d933
Sanford <sanfordzheng@gmail.com>
2019-03-09 14:27:22 -0500

    more merges completed

--- src/main/java/com/revature/controller/LoginController.java
+++ src/main/java/com/revature/controller/LoginController.java
@@ -1,33 +1,54 @@
 package com.revature.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.beans.User;
+import com.revature.service.UserService;
 
 @RestController
 @RequestMapping("/Login")
 public class LoginController {
 
-//	@Autowired
-//	UserService service;
-//	
-//	// find user by user name and then save to the session
-//	@RequestMapping(value="/{username}",
-//	    method=RequestMethod.GET,
-//		produces=MediaType.APPLICATION_JSON_VALUE)
-//	public ResponseEntity<User> findByUsername(@PathVariable String username) {
-//		
-//		// I guess this is all i need but how do we maintain session ? with this user ?? to be revealed later 
-//		User u = service.findByUsername(username);
-//		
-//		if(u==null) {
-//			//user is null, return null/no resp body with a Http status of no content
-//			return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
-//			
-//		}
-//		else {
-//			//all good, return user w status of ok
-//			return new ResponseEntity<User>(u, HttpStatus.OK);
-//			
-//		}
-//	}
+	@Autowired
+	UserService service;
+	
+	// find user by user name and then save to the session
+	@RequestMapping(value="/{username}",
+	    method=RequestMethod.GET,
+		produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> findByUsername(@PathVariable String username) {
+		// I guess this is all i need but how do we maintain session ? with this user ?? to be revealed later 
+		User u = service.findByUsername(username);
+		
+		if(u==null) {
+			//user is null, return null/no resp body with a Http status of no content
+			return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
+			
+		}
+		else {
+			//all good, return user w status of ok
+			return new ResponseEntity<User>(u, HttpStatus.OK);
+			
+		}
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> login(@RequestBody User user) {
+		User u = service.findByUsername(user.getUsername());
+		if(u == null || !u.getPassword().equals(user.getPassword())) {
+			//Username does not exist or password is incorrect
+			return new ResponseEntity<User>(HttpStatus.NO_CONTENT); 
+		} else  {
+			return new ResponseEntity<User>(u, HttpStatus.OK);
+		}
+	}
+	
 }
--- src/main/java/com/revature/controller/RegisterController.java
+++ src/main/java/com/revature/controller/RegisterController.java
@@ -1,23 +1,43 @@
 package com.revature.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.revature.beans.User;
+import com.revature.service.UserService;
+
 //@RestController
 //@RequestMapping("/Register")
 public class RegisterController {
 
-//	@Autowired
-//	UserService service;
-//	// POST
-//		@RequestMapping(method=RequestMethod.POST,
-//				consumes=MediaType.APPLICATION_JSON_VALUE,
-//				produces=MediaType.APPLICATION_JSON_VALUE)
-//		public ResponseEntity<User> add(@RequestBody String username){
-//			 //could add server side validation
-//			User u = service.findByUsername(username);
-//			if(u==null) {
-//				// good he did not exist before so he will be added 
-//				return new ResponseEntity<User>(u, HttpStatus.OK);
-//			}else {
-//				return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
-//			}
-//		}
+	@Autowired
+	UserService service;
+
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> register(@RequestBody User user) {
+		User u = service.findByUsername(user.getUsername());
+
+		if (u == null) {
+			user = service.saveUser(user);
+			return new ResponseEntity<User>(user, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<User>(HttpStatus.CONFLICT);
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<String> showInfo() {
+		return new ResponseEntity<String>("This means something is at least working!", HttpStatus.OK);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public ResponseEntity<User> addPreference(@RequestBody User user) {
+		return null;
+	}
+
 }
