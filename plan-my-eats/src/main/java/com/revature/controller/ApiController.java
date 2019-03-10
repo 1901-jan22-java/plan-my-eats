bef467ddbc348c79516c4755b5205e65f4e7d772
Edwin Faican <edwinxfaican@gmail.com>
2019-03-10 15:27:44 -0400

    Added functionality for parsing Recipe Api

--- plan-my-eats/src/main/java/com/revature/controller/ApiController.java
+++ plan-my-eats/src/main/java/com/revature/controller/ApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.revature.beans.PlaceDetailsResponse;
-import com.revature.beans.RestaurantApi;
+import com.revature.beans.RecipeDetailsResults;
 
 @CrossOrigin
 @RestController
@@ -19,7 +19,7 @@ import com.revature.beans.RestaurantApi;
 public class ApiController {
 	
 	
-	@RequestMapping(method=RequestMethod.POST, consumes=MediaType.TEXT_PLAIN_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method=RequestMethod.POST, consumes=MediaType.TEXT_PLAIN_VALUE, produces=MediaType.APPLICATION_JSON_VALUE, value="/restaurant")
 	public ResponseEntity<PlaceDetailsResponse> hitResApi(@RequestBody String params) {
 		String[] filters = params.split(";");
 		String apiUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?&location=" + filters[0] + "&radius=1500&type=restaurant&keyword=" + filters[1] + "&key=AIzaSyAv7FWb5nyCLZw9fxrpkaKLc3NS1BRGeXM";
@@ -39,5 +39,27 @@ public class ApiController {
             theException.printStackTrace();
         }
 		return new ResponseEntity<PlaceDetailsResponse>(HttpStatus.BAD_REQUEST);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, consumes=MediaType.TEXT_PLAIN_VALUE, produces=MediaType.APPLICATION_JSON_VALUE, value="/recipe")
+	public ResponseEntity<RecipeDetailsResults> hitRecApi(@RequestBody String params) {
+		String[] filters = params.split(";");
+		String apiUrl = "https://api.edamam.com/search?q=" + filters[0] + "&app_id=3ee293b7&app_key=5143a3f492a353eb02eac1fac6912dbc&from=0&to=3&calories=" + filters[1] + "&health=" + filters[2];
+		System.out.println(apiUrl);
+		
+		try {
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<RecipeDetailsResults> responseEntity = restTemplate.getForEntity(apiUrl, RecipeDetailsResults.class);
+            RecipeDetailsResults recipes = responseEntity.getBody();
+            if (responseEntity.getStatusCode().toString().equals("200")) {
+                return new ResponseEntity<RecipeDetailsResults>(recipes, HttpStatus.OK);
+            } else {
+            	return new ResponseEntity<RecipeDetailsResults>(HttpStatus.BAD_REQUEST);
+            }
+
+        } catch (Exception theException) {
+            theException.printStackTrace();
+        }
+		return new ResponseEntity<RecipeDetailsResults>(HttpStatus.BAD_REQUEST);
 	}
 }
