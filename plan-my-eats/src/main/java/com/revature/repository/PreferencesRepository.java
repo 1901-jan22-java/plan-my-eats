3b1caf926d267cc96cbbfecb3140421e125e13a6
DaysiZ <daysi.granados@stonybrook.edu>
2019-03-11 10:06:22 -0400

    all the stuff

--- plan-my-eats/src/main/java/com/revature/repository/PreferencesRepository.java
+++ plan-my-eats/src/main/java/com/revature/repository/PreferencesRepository.java
@@ -9,6 +9,9 @@ import com.revature.beans.Preferences;
 @Repository
 public interface PreferencesRepository extends JpaRepository<Preferences,Integer>  {
  
+	
+	//update preferences
+	
 	@Query("SELECT p FROM Preferences p WHERE p.name = ?1")
 	Preferences findByName(String name);
 }
