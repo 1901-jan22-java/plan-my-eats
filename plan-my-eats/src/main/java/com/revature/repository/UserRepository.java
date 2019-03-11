a6e085e7c8c5ec0638445469e8c35bd49eab569b
DaysiZ <daysi.granados@stonybrook.edu>
2019-03-11 19:05:37 -0400

    merge 4 real

--- plan-my-eats/src/main/java/com/revature/repository/UserRepository.java
+++ plan-my-eats/src/main/java/com/revature/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.revature.repository;
 
 import org.springframework.data.repository.query.Param;
 
-<<<<<<< HEAD
 import com.revature.beans.Preferences;
 import com.revature.beans.Restaurant;
 import com.revature.beans.User;
@@ -10,11 +9,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import java.util.List;
 import java.util.Set;
 
-=======
-import com.revature.beans.User;
-import org.springframework.data.jpa.repository.Modifying;
-import java.util.List;
->>>>>>> Edwin
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -24,7 +18,6 @@ public interface UserRepository extends JpaRepository<User,Integer>{
 
 	@Query("SELECT u from User u WHERE u.username = ?1")
 	User findByUsername(String username);
-<<<<<<< HEAD
 
 
 	@Query(value="SELECT u from User WHERE u.restaurant LIKE '%':restaurant'%'")
@@ -37,8 +30,6 @@ public interface UserRepository extends JpaRepository<User,Integer>{
    @Modifying
    @Query("update User u set u.preferences=?1 WHERE u.username = ?2")
     User setUserPreferenceByUsername(String preference,String username);
-=======
->>>>>>> Edwin
    
    @Modifying
    @Query("update User u set u.preferences=?1 WHERE u.userId = ?2")
@@ -60,7 +51,6 @@ public interface UserRepository extends JpaRepository<User,Integer>{
    @Query("update User u set u.gender=?1 WHERE u.username = ?2")
     User setUserGenderByUsername(String gender,String username);
    
-<<<<<<< HEAD
    @Modifying
    @Query("update User u set u.height=?1,u.weight=?2,u.gender=?3,u.age=?4 WHERE u.username = ?5")
     User setUserInfoByUsername(int height, double weight, String gender, int age,String username);
@@ -71,7 +61,6 @@ public interface UserRepository extends JpaRepository<User,Integer>{
    
    
    
-=======
 //   @Modifying
 //   @Query("update User u set u.height=?1,u.weight=?2,u.gender=?3,u.age=?4 WHERE u.username = ?5")
 //    User setUserInfoByUsername(int height, double weight, String gender, int age,String username);
@@ -79,7 +68,6 @@ public interface UserRepository extends JpaRepository<User,Integer>{
 //   @Modifying
 //   @Query("update User u set u.preference =?1 u.height=?2,u.weight=?3,u.gender=?4,u.age=?5 WHERE u.username = ?6")
 //    User setUserAllInfoByUsername(String preference,int height, double weight, String gender, int age,String username);
->>>>>>> Edwin
 }
 
 
