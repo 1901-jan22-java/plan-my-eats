1c1dffb6a40d565a4c4fe7966f87962fd73eccb5
Sanford Zheng <sanfordzheng@gmail.com>
2019-03-12 16:11:29 -0400

    CORS fixed

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.beans.User;
 import com.revature.services.UserService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/login")
 public class LoginController {
--- src/main/java/com/revature/controllers/RegisterController.java
+++ src/main/java/com/revature/controllers/RegisterController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,11 +14,17 @@ import com.revature.beans.User;
 import com.revature.services.UserService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/register")
 public class RegisterController {
 
 	@Autowired
 	UserService service;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<String> showInfo() {
+		return new ResponseEntity<String>("This means something is at least working!", HttpStatus.OK);
+	}
 
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> register(@RequestBody User user) {
@@ -31,11 +38,6 @@ public class RegisterController {
 			return new ResponseEntity<User>(HttpStatus.CONFLICT);
 		}
 
-	}
-
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<String> showInfo() {
-		return new ResponseEntity<String>("This means something is at least working!", HttpStatus.OK);
 	}
 
 	@RequestMapping(method = RequestMethod.PUT)
