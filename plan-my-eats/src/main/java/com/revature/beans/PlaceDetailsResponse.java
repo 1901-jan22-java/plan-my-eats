110f1f93ef06c315990e13e0289691e85eecda32
Edwin Faican <edwinxfaican@gmail.com>
2019-03-16 23:01:29 -0400

    Changing file structure

--- plan-my-eats/src/main/java/com/revature/beans/PlaceDetailsResponse.java
+++ plan-my-eats/src/main/java/com/revature/beans/PlaceDetailsResponse.java
@@ -36,9 +36,13 @@ public class PlaceDetailsResponse {
 
 	public List<String> PhotosList() {
 		List<String> photo = new ArrayList<String>();
-;
+		
 		for(PlaceDetails p : results) {
-			photo.add(p.getPhotos().get(0).getReference());
+			if(p.getPhotos().size() > 0) {
+				photo.add(p.getPhotos().get(0).getReference());
+			} else {
+				photo.add("No Photo");
+			}
 		}
 		return photo;
 	}
