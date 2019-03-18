e677b6872aef65fa473590e4e36e5b7db9edf027
Edwin Faican <edwinxfaican@gmail.com>
2019-03-18 00:08:07 -0400

    Adding new recipe support

--- src/main/java/com/revature/controllers/RecipeController.java
+++ src/main/java/com/revature/controllers/RecipeController.java
@@ -20,6 +20,7 @@ import com.revature.beans.Preference;
 import com.revature.beans.Recipe;
 import com.revature.beans.User;
 import com.revature.dtos.RecipeDetailsResults;
+import com.revature.dtos.RecipeResults;
 import com.revature.services.RecipeService;
 import com.revature.services.UserService;
 
@@ -35,10 +36,19 @@ public class RecipeController {
 	@Autowired
 	RecipeService rs;
 
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<RecipeDetailsResults> hitRecApi(@RequestBody User u) {
-		Random rand = new Random();
-		String[] filters = new String[2];
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Recipe>> hitRecApi(@RequestBody User u) {
+		
+		List<String> filters = new ArrayList<String>();
+		filters.add("pasta");
+		filters.add("rice");
+		filters.add("sauce");
+		filters.add("cake");
+		filters.add("pastry");
+		filters.add("bread");
+		filters.add("eggs");
+		
+		int rand = (int) Math.floor(Math.random()*filters.size());
 		double calories = 0;
 
 		/*
@@ -49,55 +59,58 @@ public class RecipeController {
 		 * (4.6756 x age in years)
 		 * 
 		 */
-		if (u.getGender() == "male") {
-			calories = 66.4730d + (13.7516 * (u.getWeight() * 0.4539d)) + (5.0033d * (u.getHeight() * 2.54d))
-					- (6.7550 * u.getAge());
+		if (u.getGender().equalsIgnoreCase("male")) {
+			calories = 66.4730d + (13.7516d * (u.getWeight() * 0.4539d)) + (5.0033d * (u.getHeight() * 2.54d))
+					- (6.7550d * u.getAge());
 			calories *= 1.2d;
-		} else if (u.getGender() == "female") {
+		} else if (u.getGender().equalsIgnoreCase("female")) {
 			calories = 655.0955d + (9.5634d * (u.getWeight() * 0.4539d)) + (1.8496d * (u.getHeight() * 2.54d))
 					- (4.6756d * u.getAge());
 			calories *= 1.2d;
 		}
-		if (u.getPreferences().size() > 3) {
-			// Keep track of an index
-			int firstIndex = -1;
-			// Limit the number of tries the loop can go through
-			int tries = u.getPreferences().size() * 2;
-			// Try to get 2 filters
-			for (int i = 0; i < 2; i++) {
-				// If you run out of tries exit to not be stuck forever
-				if (tries == 0)
-					break;
-				// Get a random index that hasn't been used before
-				int index = rand.nextInt(u.getPreferences().size());
-				if (index == firstIndex) {
-					i--;
-				} else {
-					firstIndex = index;
-					List<Preference> li = new ArrayList<Preference>(u.getPreferences());
-					Preference pref = li.get(index);
-					if (pref.getPrefId() > 19) {
-						tries--;
-						i--;
-					} else {
-						filters[i] = pref.getName();
-					}
-				}
-			}
-		} else {
-			List<Preference> li = new ArrayList<Preference>(u.getPreferences());
-			if (li.size() != 0) {
-				if (li.get(0).getPrefId() < 20) {
-					filters[0] = li.get(0).getName();
-				}
-				if (li.size() == 2 && li.get(1).getPrefId() < 20) {
-					filters[1] = li.get(1).getName();
-				}
-			}
-		}
-		String apiUrl = "https://api.edamam.com/search?q="
+		String calorieCount = (int)((calories/3)-100) + "-" + (int)((calories/3)+100);
+		
+		
+//		if (u.getPreferences().size() > 3) {
+//			// Keep track of an index
+//			int firstIndex = -1;
+//			// Limit the number of tries the loop can go through
+//			int tries = u.getPreferences().size() * 2;
+//			// Try to get 2 filters
+//			for (int i = 0; i < 2; i++) {
+//				// If you run out of tries exit to not be stuck forever
+//				if (tries == 0)
+//					break;
+//				// Get a random index that hasn't been used before
+//				int index = rand.nextInt(u.getPreferences().size());
+//				if (index == firstIndex) {
+//					i--;
+//				} else {
+//					firstIndex = index;
+//					List<Preference> li = new ArrayList<Preference>(u.getPreferences());
+//					Preference pref = li.get(index);
+//					if (pref.getPrefId() > 19) {
+//						tries--;
+//						i--;
+//					} else {
+//						filters[i] = pref.getName();
+//					}
+//				}
+//			}
+//		} else {
+//			List<Preference> li = new ArrayList<Preference>(u.getPreferences());
+//			if (li.size() != 0) {
+//				if (li.get(0).getPrefId() < 20) {
+//					filters[0] = li.get(0).getName();
+//				}
+//				if (li.size() == 2 && li.get(1).getPrefId() < 20) {
+//					filters[1] = li.get(1).getName();
+//				}
+//			}
+//		}
+		String apiUrl = "https://api.edamam.com/search?q=" + filters.get(rand) 
 				+ "&app_id=3ee293b7&app_key=5143a3f492a353eb02eac1fac6912dbc&from=0&to=3&calories="
-				+ (int) (calories * 0.33d) + "&health=" + filters[0] + "+" + filters[1];
+				+ calorieCount ;
 		log.info(apiUrl);
 
 		try {
@@ -105,26 +118,28 @@ public class RecipeController {
 			ResponseEntity<RecipeDetailsResults> responseEntity = restTemplate.getForEntity(apiUrl,
 					RecipeDetailsResults.class);
 			RecipeDetailsResults recipes = responseEntity.getBody();
+
 			List<Recipe> recp = new ArrayList<Recipe>();
-			for (int i = 0; i < 20; i++) {
+			for (int i = 0; i < recipes.getHits().size(); i++) {
 				Recipe r = new Recipe();
 				r.setCalories(recipes.getCalories(i) / recipes.getServings(i));
-				for (int j = 0; j < recipes.getIngredients(i).size(); j++) {
-					r.setIngredients(r.getIngredients() + recipes.getIngredients(i).get(j) + ".");
-				}
+//				for (int j = 0; j < recipes.getIngredients(i).size(); j++) {
+//					r.setIngredients(r.getIngredients() + recipes.getIngredients(i).get(j) + ".");
+//				}
+				r.setIngredients(recipes.getIngredients(i).toString());
 				r.setRecipeName(recipes.getName(i));
 				recp.add(r);
 			}
 			if (responseEntity.getStatusCode().toString().equals("200")) {
-				return new ResponseEntity<RecipeDetailsResults>(recipes, HttpStatus.OK);
+				return new ResponseEntity<List<Recipe>>(recp, HttpStatus.OK);
 			} else {
-				return new ResponseEntity<RecipeDetailsResults>(HttpStatus.BAD_REQUEST);
+				return new ResponseEntity<List<Recipe>>(HttpStatus.BAD_REQUEST);
 			}
 
 		} catch (Exception e) {
 			log.error("", e);
 		}
-		return new ResponseEntity<RecipeDetailsResults>(HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<List<Recipe>>(HttpStatus.BAD_REQUEST);
 	}
 
 	@RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
